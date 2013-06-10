25cdea51100e8072f8c51347dca4234c23ebd1e0
achmad.ha <achmad.ha@M06065L11.astra-honda.com>
2013-06-10 10:42:50 +0700

    Revert "test remove"
    This reverts commit 775ade04df8c24c97ebf9d8ab57b0dd4fb3017ca.

---
+++ TestDuluAja1/src/com/TestClass1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author achmad.ha
+ */
+public class TestClass1 {
+    
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+    
+    
+    
+}
