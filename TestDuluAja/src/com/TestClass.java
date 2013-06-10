eb43d0c17f873132c3832cd541beea3796cd3a2c
achmad.ha <achmad.ha@M06065L11.astra-honda.com>
2013-06-10 10:24:39 +0700

    done

--- TestDuluAja/src/com/TestClass.java
+++ TestDuluAja/src/com/TestClass.java
@@ -10,4 +10,14 @@ package com;
  */
 public class TestClass {
     
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+    
 }
