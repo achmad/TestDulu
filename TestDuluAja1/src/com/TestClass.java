30e37c8f5832ececc8c3040c455570f2782044d1
achmad.ha <achmad.ha@M06065L11.astra-honda.com>
2013-06-10 10:47:58 +0700

    Revert "menambahkan field test1 di TestClass"
    This reverts commit aaba936a23a150c91567a939d99bb0b69a3f042c.

--- TestDuluAja1/src/com/TestClass.java
+++ TestDuluAja1/src/com/TestClass.java
@@ -11,17 +11,6 @@ package com;
 public class TestClass {
     
     private String test;
-    private String test1;
-
-    public String getTest1() {
-        return test1;
-    }
-
-    public void setTest1(String test1) {
-        this.test1 = test1;
-    }
-    
-    
 
     public String getTest() {
         return test;
