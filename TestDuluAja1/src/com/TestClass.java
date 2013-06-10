aaba936a23a150c91567a939d99bb0b69a3f042c
achmad.ha <achmad.ha@M06065L11.astra-honda.com>
2013-06-10 10:47:17 +0700

    menambahkan field test1 di TestClass

--- TestDuluAja1/src/com/TestClass.java
+++ TestDuluAja1/src/com/TestClass.java
@@ -11,6 +11,17 @@ package com;
 public class TestClass {
     
     private String test;
+    private String test1;
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public void setTest1(String test1) {
+        this.test1 = test1;
+    }
+    
+    
 
     public String getTest() {
         return test;
